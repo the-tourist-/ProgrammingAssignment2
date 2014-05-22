817c07d71fbc7b9ea2d963497b2040689a9c04e9
Graeme <graemesmith70@gmail.com>
2014-05-23 01:44:39 +0200

    Completion of Programming Assignment
    Added function bodies for makeCacheMatrix and solveCache.  Also added
    some testing functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The following will create a nxn matrix and seed it with random 
+## normal values with a Mean of 0 and SD of 1.  It is to assist testing
+
+randomMatrix <- function(n) {
+    matrix(rnorm(n^2), n, n)
+}
+
+## The following will create a list of functions that allow for a 
+## vector to be set or get, and also the inverse of that matrix to
+## be set or get.  It also contains an internal memory that will
+## cache the inverse matrix so that it doesn't need to be recalculated
+## if it has already been calculated
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                                           
+    mem <- NULL
+    set <- function(y) {
+        x <<- y
+        mem <<- NULL
+    }
+    get <- function() x
+    setInverseMatrix <- function(inverseX) mem <<- inverseX
+    getInverseMatrix <- function() mem
+    list(set = set, get = get,
+         setInverseMatrix = setInverseMatrix,
+         getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function will first check in the memory of the CacheMatrix to see if
+## the inverse has already been returned and return that if so.  If not it
+## will calculate the inverse and store it in the CacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    mem <- x$getInverseMatrix()
+    if(!is.null(mem)) {
+        message("getting cached data")
+        return(mem)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverseMatrix(m)
+    m
+}
+
+# This is a quick test that uses a timer on a large matrix to see if the cache is being used
+
+timeCacheSolveTest  <- function() {
+    m <- randomMatrix(1000)
+    cm <- makeCacheMatrix(m)
+    
+    t1 = system.time (mi1<-cacheSolve(cm))
+    t2 = system.time (mi2<-cacheSolve(cm))
+    
+    list(isInverse=round(sum(as.vector(m %*% mi2)), 5)==1000,timer1=t1,timer2=t2)
 }
